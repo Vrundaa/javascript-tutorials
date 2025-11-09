af230da148f227bb940e794a2db8e623c5a01191
vidyasurya <41923217+Vrundaa@users.noreply.github.com>
2025-11-09 15:20:16 +0000

    Conversions of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,28 @@
-let score = 33;
+let score = "33aa";
+console.log(typeof score); // string
+let valueInNumber = Number (score);
+console.log(valueInNumber); // NaN
+console.log(typeof valueInNumber); // number
 
-console.log(typeof score);
+let score1 = null;
+console.log(typeof score1); // object
+let valueInNumber1 = Number (score1);
+console.log(valueInNumber1); // 0
+console.log(typeof valueInNumber1); // number
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1
+// false ==> 0
+// null ==> object
+
+let isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolenIsLoggedIn);
+console.log(boolenIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "some text" => true
+
