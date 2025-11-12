f0a5090075124654ec14668ebec80f81823fe9c0
vidyasurya <41923217+Vrundaa@users.noreply.github.com>
2025-11-12 14:30:11 +0000

    Array 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,21 @@
+const arr1 =[0,1,2,3,4];
+const arr2 =[5,6,7,8,9];
+
+//arr1.push(arr2);
+//console.log(arr1); //[ 0, 1, 2, 3, 4, [ 5, 6, 7, 8, 9 ] ]  Adds arrays a new item 
+
+//const arr3= arr1.concat(arr2); // returns new array
+//console.log(arr3); // [  0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//const allitems=[...arr1,...arr2]; // using spread operator
+//console.log(allitems); //[ 0, 1, 2, 3, 4, [ 5, 6, 7, 8, 9 ] ]
+
+// const arr5=[1,2,3,[4,5],6,[7,[8,9]]];
+// console.log(arr5); // [ 1, 2, 3, [ 4, 5 ], 6, [ 7, [ 8, 9 ] ] ]
+
+// const flatarr=arr5.flat(Infinity);
+// console.log(flatarr); // [  1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+ 
+
