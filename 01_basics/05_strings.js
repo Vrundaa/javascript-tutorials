75b2430570809d83b2a4bb04cbe8aaa259888632
vidyasurya <41923217+Vrundaa@users.noreply.github.com>
2025-11-12 10:44:59 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,6 +1,13 @@
-const name ="test name";
+const Myname ="TestName";
 const repoCount=50;
 
 // Use backstash for string interpolation
 
-console.log(`Hello my name is ${name} and I have these repositroeis ${repoCountnOT}`);
+console.log(`Hello my name is ${Myname} and I have these repositroeis ${repoCount}`);
+
+const newString=Myname.substring(0,5);
+console.log(newString); // TestN
+
+const anotherString=Myname.slice(-8,4);
+console.log(anotherString); // Test
+
