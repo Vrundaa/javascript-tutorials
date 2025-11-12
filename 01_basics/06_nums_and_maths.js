3a503788df006c6d8e495d152fa755e2c853c37e
vidyasurya <41923217+Vrundaa@users.noreply.github.com>
2025-11-12 13:12:02 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,29 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100.8888);
+//console.log(balance);
+
+//console.log(balance.toString()); // 100.8888
+//console.log(balance.toFixed(2)); // 100.89
+
+const otherNum=123.8966;
+//console.log(otherNum.toPrecision(3)); //125
+
+const otherNum1=1123.8966;
+//console.log(otherNum1.toPrecision(3)); //1.12e+3
+
+const hunders = 1000000;
+//console.log(hunders.toLocaleString('en-IN'));
+
+/**************** Maths *********************/
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.random()); // comes values between 0 - 1
+console.log(Math.floor((Math.random()*10))+1);
+
+const min =10;
+const max =20;
+
+console.log( Math.floor((Math.random() * (max - min + 1) + min)));
+
